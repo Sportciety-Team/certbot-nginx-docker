37e122c01c85e780703df01aecf9394d1f46cba6
User <user@Nicolocos-Macbook-Pro.local>
2024-10-28 13:56:03 +0200

    nginx

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -5,7 +5,7 @@ if ! [ -x "$(command -v docker compose)" ]; then
   exit 1
 fi
 
-domains=(api.sportcietyapp.com)
+domains=(server.sportcietyapp.com)
 rsa_key_size=4096
 data_path="./data/certbot"
 email="dev@sportcietyapp.com" # Adding a valid address is strongly recommended
